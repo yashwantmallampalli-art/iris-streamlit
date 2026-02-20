309295cb6b41131514b5ab668237c9737378b163
yashwantmallampalli-art <yashwantmallampalli@gmail.com>
2026-02-20 11:51:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
-# Install streamlit if not already installed
-!pip install streamlit
-
 import streamlit as st
 from sklearn.datasets import load_iris
 data = load_iris()
-
 from sklearn.ensemble import RandomForestClassifier
 model = RandomForestClassifier()
 x = data.data
 y = data.target
 model.fit(x,y)
-
 st.header('Iris Flower classification')
 sl = st.number_input('Enter Sepal Length')
 sw = st.number_input('Enter Sepal Width')
